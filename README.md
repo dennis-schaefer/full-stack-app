ee64347bcdd3abf66f09c306a9f53293494a6a6e
Dennis Schäfer <dennis.schaefer94@gmail.com>
2025-11-22 10:50:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Notes
+- JEP 519 (Compact Object Headers) currently as opt-in with `java -XX:+UseCompactObjectHeaders -jar app.jar`
+- 
---
+++ src/main/java/de/schaeferd/fullstackapp/FullStackAppApplication.java
@@ -0,0 +1,13 @@
+package de.schaeferd.fullstackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAppApplication
+{
+    static void main(String[] args)
+    {
+        SpringApplication.run(FullStackAppApplication.class, args);
+    }
+}
