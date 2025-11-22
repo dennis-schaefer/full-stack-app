1481d33c6f5a35b2e2eaf0a17f95e58863a176dd
Dennis Schäfer <dennis.schaefer94@gmail.com>
2025-11-22 15:34:24 +0100

    Added Spring Security OAuth2 Client

---
+++ src/main/java/de/schaeferd/fullstackapp/security/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package de.schaeferd.fullstackapp.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.server.servlet.CookieSameSiteSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration
+{
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http
+            .oauth2Login(login -> login
+                    .loginPage("/oauth2/authorization/keycloak")
+                    .defaultSuccessUrl("/")
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    CookieSameSiteSupplier sameSiteSupplier()
+    {
+        return CookieSameSiteSupplier.ofLax();
+    }
+}
