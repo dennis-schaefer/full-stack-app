1b6b8cbec395058862e75511521406ab088fd9e4
Dennis Schäfer <dennis.schaefer94@gmail.com>
2025-11-22 16:52:47 +0100

    Implemented build of JAR with bundled frontend

--- src/main/frontend/vite.config.ts
+++ src/main/frontend/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false
+            },
+            '/ws': {
+                target: 'ws://localhost:8080',
+                ws: true,
+                secure: false
+            }
+        }
+    },
+    build: {
+        outDir: '../resources/static/',
+        emptyOutDir: true,
+    },
 })
