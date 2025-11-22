07b2b7e0e28633bd82d4072d236c3c596850a946
Dennis Schäfer <dennis.schaefer94@gmail.com>
2025-11-22 16:55:40 +0100

    Added configuration to enable routing to static frontend resources

---
+++ src/main/java/de/schaeferd/fullstackapp/frontend/RoutingConfiguration.java
@@ -0,0 +1,43 @@
+package de.schaeferd.fullstackapp.frontend;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+
+@Configuration
+public class RoutingConfiguration implements WebMvcConfigurer
+{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        var endpoint = "/";
+        var location = "classpath:/static/";
+
+        var endpointPatterns = new String[] {endpoint, endpoint + "**"};
+        registry
+                .addResourceHandler(endpointPatterns)
+                .addResourceLocations(location)
+                .resourceChain(false)
+                .addResolver(new PathResourceResolver()
+                {
+                    public Resource resolveResource(HttpServletRequest request, @NonNull String requestPath,
+                                                    @NonNull List<? extends Resource> locations, @NonNull ResourceResolverChain chain)
+                    {
+                        var resource = super.resolveResource(request, requestPath, locations, chain);
+                        if (nonNull(resource))
+                            return resource;
+
+                        return super.resolveResource(request, "/index.html", locations, chain);
+                    }
+                });
+    }
+}
